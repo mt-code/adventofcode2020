f0c893a0271a8cea6f697f6ff8f6f44c88442386
mt-code <matthew.croston@btinternet.com>
2020-12-03 16:09:40 +0000

    Completed day 01 challenge solution

---
+++ day-01/day-01.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal("File read error: ", err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	for i := 0; i < len(lines); i++ {
+		firstNum, _ := strconv.Atoi(lines[i])
+
+		for k := i + 1; k < len(lines); k++ {
+			secondNum, _ := strconv.Atoi(lines[k])
+
+			if (firstNum + secondNum) == 2020 {
+				fmt.Println("#1: ", firstNum)
+				fmt.Println("#2: ", secondNum)
+				fmt.Println("#1 * #2 = ", firstNum*secondNum)
+				return
+			}
+		}
+	}
+}
